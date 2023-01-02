10710662ea5d2eedf014ebcb6a17dd2298b5e4af
Norman Chan <herbtactics@gmail.com>
2023-01-02 13:39:05 -0800

    stuff

--- youtube/Main.java
+++ youtube/Main.java
@@ -1,8 +1,23 @@
 package youtube;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello World");        
+        try (Scanner dirInput = new Scanner(System.in)) {
+            System.out.println("Enter directory");
+
+            String userName = dirInput.nextLine();
+            File directory = new File(userName);
+            File[] list = directory.listFiles();
+            for (int i = 0; i < list.length; i++) {
+                System.out.println(list[i]);
+                
+            }
+        }
     }
 }
+
+// create a java program that can play music in the background given a folder of music videos(for android phone)
